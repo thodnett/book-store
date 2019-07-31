42ffc664c54af8efbe0e00059d487e34329a7206
Ubuntu <ubuntu@ip-172-31-82-225.ec2.internal>
2019-07-31 15:37:47 +0000

    added books and addbook

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask,  render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'book_store'
+app.config["MONGO_URI"] = 'mongodb+srv://thodnett:Pass1234@myfirstcluster-2pyxt.mongodb.net/book_store?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_books')
+def get_books():
+    return render_template("books.html", books=mongo.db.books.find())
+    
+@app.route('/add_book')
+def add_book():
+    return render_template("addbook.html")
 
 
 if __name__ == '__main__':
